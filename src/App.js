5e04bab2d9e501a3973a2192eeb485695e344134
Ryan Mendoza <71746816+ryan-mendoza@users.noreply.github.com>
2021-06-13 22:17:10 -0700

    added api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+      )
+      .then((res) => {
+        setCoins(res.data);
+        console.log('res.data');
+      })
+      .catch((error) => console.log('error!'));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='coin__app'>
+      {/* search bar on the top */}
+      <div className='coin__search'>
+        <h1 className='coin__text'>Seach a currency</h1>
+        <form>
+          <input type='text' placeholder='Search' className='coin__input' />
+        </form>
+      </div>
     </div>
   );
 }
