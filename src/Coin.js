7550e44ae58c5bdb21c1344ecf1458e0880c7ba4
Ryan Mendoza <71746816+ryan-mendoza@users.noreply.github.com>
2021-06-13 23:27:13 -0700

    completed the api data

--- src/Coin.js
+++ src/Coin.js
@@ -1,17 +1,33 @@
 import React from 'react';
 
-const Coin = ({ name, image, symbol, price, volume }) => {
+const Coin = ({
+  name,
+  image,
+  symbol,
+  price,
+  volume,
+  priceChange,
+  marketcap,
+}) => {
   return (
     <div className='coin__container'>
       <div className='coin__row'>
         <div className='coin'>
-          <img src='{image}' alt='crypto' />
+          <img src={image} alt='crypto' />
           <h1>{name}</h1>
           <p className='coin__symbol'>{symbol}</p>
         </div>
         <div className='coin__data'>
           <p className='coin__price'>${price}</p>
           <p className='coin__volume'>${volume.toLocaleString()}</p>
+          {priceChange < 0 ? (
+            <p className='coin__percent red'>{priceChange.toFixed(2)}%</p>
+          ) : (
+            <p className='coin__percent green'>{priceChange.toFixed(2)}%</p>
+          )}
+          <p className='coin__marketcap'>
+            Mkt Cap: ${marketcap.toLocaleString()}
+          </p>
         </div>
       </div>
     </div>
